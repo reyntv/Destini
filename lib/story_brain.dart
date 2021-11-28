4403a029db434a70e805ab8dcb80e794f9f4a00e
Reynir <reynirtt@gmail.com>
2021-11-28 23:29:55 +0000

    all steps after deadline

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -33,52 +33,46 @@ class StoryBrain {
         choice2: '')
   ];
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
   String getStory() {
-    return _storyData.first.storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
   String getChoice1() {
-    return _storyData.first.choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
   String getChoice2() {
-    return _storyData.first.choice2;
+    return _storyData[_storyNumber].choice2;
   }
 
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-  int storyNumber = 0;
+  int _storyNumber = 0;
 
   void nextStory(int choiceNumber) {
-    if (storyNumber == 0 && choiceNumber == 1) {
-      storyNumber = 2;
-    }
-    if (storyNumber == 0 && choiceNumber == 2) {
-      storyNumber = 1;
-    }
-    if (storyNumber == 2 && choiceNumber == 1) {
-      storyNumber = 5;
-    }
-    if (storyNumber == 2 && choiceNumber == 2) {
-      storyNumber = 4;
-    }
-    if (storyNumber == 1 && choiceNumber == 1) {
-      storyNumber = 2;
-    }
-    if (storyNumber == 1 && choiceNumber == 2) {
-      storyNumber = 3;
+    if ([3, 4, 5].contains(_storyNumber)) {
+      restart();
+      return;
     }
 
-    if ([3, 4, 5].contains(storyNumber)) {
-      restart();
+    if (_storyNumber == 0 && choiceNumber == 1) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 0 && choiceNumber == 2) {
+      _storyNumber = 1;
+    } else if (_storyNumber == 2 && choiceNumber == 1) {
+      _storyNumber = 5;
+    } else if (_storyNumber == 2 && choiceNumber == 2) {
+      _storyNumber = 4;
+    } else if (_storyNumber == 1 && choiceNumber == 1) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 1 && choiceNumber == 2) {
+      _storyNumber = 3;
     }
   }
 
   void restart() {
-    storyNumber = 0;
+    _storyNumber = 0;
   }
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    return [0, 1, 2].contains(_storyNumber);
+  }
 }
